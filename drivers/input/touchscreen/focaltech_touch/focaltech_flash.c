d5962151f6493ad179bd418ded76206da1df944e
ruyue <ruyueee@outlook.com>
2025-03-23 22:58:02 +0800

    drivers/input/touchscreen/focaltech_touch/focaltech_flash.c: fixup drop nothing codes

--- drivers/input/touchscreen/focaltech_touch/focaltech_flash.c
+++ drivers/input/touchscreen/focaltech_touch/focaltech_flash.c
@@ -939,21 +939,6 @@ int fts_ctpm_lcd_cfg_upgrade(struct i2c_client *client)
 
 	return i_ret;
 }
-
-static int check_chip_id(u8 chip_id1, u8 chip_id2)
-{
-#if FTS_CHIP_IDC
-	if ((chip_id1 == chip_types.chip_idh)
-			&& (chip_id2 == chip_types.chip_idl) {
-#else
-	if (chip_id1 == chip_types.chip_idh) {
-#endif
-		return 1;
-	}
-	return 0;
-}
-
-#if (!(FTS_UPGRADE_STRESS_TEST))
 /************************************************************************
  * Name: fts_flash_read_buf
  * Brief: read data from flash
